ea3e26a153d863beb0be6059886b8cc713048caf
Jordan Smithson <jordantroysmithson@gmail.com>
2018-08-18 15:36:22 -0600

    solved drills 1 and 2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+export default class App extends Component {
+
+constructor (prop) {
+  super (prop)
+    this.state = {
+      sodaBrands: [
+        'Coke',
+        'Pepsi',
+        'Shasta',
+        'la Croix'
+      ]
+    }
 }
 
-export default App;
+render () {
+
+let kindsOfSoda = this.state.sodaBrands.map((element, index) => {
+  return <h1 key = {index}>{element}</h1>
+})
+
+  return (
+    <div className = 'App'>
+      {kindsOfSoda}
+    </div>
+  )
+}
+
+}
